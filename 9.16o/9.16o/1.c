7376b425274a03fb5dacba33d7184802b526aa11
uh-uh-fw <3486039440@qq.com>
2025-09-16 22:06:39 +0800

    one

---
+++ 9.16o/9.16o/1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define name "sd" 
+#define surname "Y"
+
+int main(void)
+{
+
+	printf("%s%s\n", surname, name);
+	printf("%s\n", name);
+	printf("%s\n", surname);
+	printf("%s%s\n", surname, name);
+	return 0;
+
+
+
+}
---
+++ 9.16y/9.16y/2.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int jolly(void);
+int deny(void);
+int main(void)
+{
+	jolly();
+	jolly();
+	jolly();
+	deny();
+	return 0;
+}
+int jolly(void)
+{
+	printf("For he is a jolly good fellow£¡\n");
+	return 0;
+}
+int deny(void)
+{
+	printf("Which nobody can deny£¡\n");
+	return 0;
+}
+
+
+
+
+
