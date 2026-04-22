ca624b785515642f831f875aeb3661b05cc69d92
uh-uh-fw <3486039440@qq.com>
2026-04-22 23:11:17 +0800

    Codeforces Round 1090 (Div. 4) Editorial E. The 67th XOR Problem

---
+++ 4.22cf/4.22cf/main.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 4000;
+int a[N];
+int main()
+{
+	int T; cin >> T;
+	while (T--)
+	{
+		int ret=-0x3f3f3f3f;
+		int n; cin >> n;
+		int x = 0;
+		int flag = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> a[i];
+		}
+		for (int i = 1; i <= n; i++)
+		{
+			int x = a[i];
+			for (int j = i+1; j <= n; j++)
+			{
+				ret = max(ret, a[i] ^ a[j]);
+			}
+			
+		}
+		cout <<ret << endl;
+		
+	}
+
+	return 0;
+}
