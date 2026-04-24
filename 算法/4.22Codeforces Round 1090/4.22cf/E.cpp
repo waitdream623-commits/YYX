029b952c27ab71a398d77b7ef646da47ffa22b5a
uh-uh-fw <3486039440@qq.com>
2026-04-24 23:07:23 +0800

    累了，跑步爽

--- 算法/4.22Codeforces Round 1090/4.22cf/E.cpp
+++ 算法/4.22Codeforces Round 1090/4.22cf/E.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 const int N = 4000;
 int a[N];
-////int main()
-//{
-//	int T; cin >> T;
-//	while (T--)
-//	{
-//		int ret=-0x3f3f3f3f;
-//		int n; cin >> n;
-//		int x = 0;
-//		int flag = 1;
-//		for (int i = 1; i <= n; i++)
-//		{
-//			cin >> a[i];
-//		}
-//		for (int i = 1; i <= n; i++)
-//		{
-//			int x = a[i];
-//			for (int j = i+1; j <= n; j++)
-//			{
-//				ret = max(ret, a[i] ^ a[j]);
-//			}
-//			
-//		}
-//		cout <<ret << endl;
-//		
-//	}
-//
-//	return 0;
-//}
+//int main()
+{
+	int T; cin >> T;
+	while (T--)
+	{
+		int ret=-0x3f3f3f3f;
+		int n; cin >> n;
+		int x = 0;
+		int flag = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> a[i];
+		}
+		for (int i = 1; i <= n; i++)
+		{
+			int x = a[i];
+			for (int j = i+1; j <= n; j++)
+			{
+				ret = max(ret, a[i] ^ a[j]);
+			}
+			
+		}
+		cout <<ret << endl;
+		
+	}
+
+	return 0;
+}
