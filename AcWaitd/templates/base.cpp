d7ef44b92d627185b60ee6f087db0b98a8c97f3e
uh-uh-fw <3486039440@qq.com>
2026-04-29 22:56:17 +0800

    单调队列，双端求区间最大最小

--- AcWaitd/templates/base.cpp
+++ AcWaitd/templates/base.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 typedef long long ll;
-const int N=1e6+10;
+const int N = 1e6 + 10;
 int main() {
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
+  
   return 0;
 }
