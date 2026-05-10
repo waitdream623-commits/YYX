3b0167fdda798c0957b1b3df73999aeefd295fb9
uh-uh-fw <3486039440@qq.com>
2026-05-10 22:23:04 +0800

    今天参加了守护生命 善行郑道的欢乐跑活动，5人一组，活动3个小时左右，很快乐，原来我也不是很社恐，活动交友，心灵排毒，身体排毒。

--- AcWaitd/Graph_Theory/Multi-Source_Shortest_Path/P_2910_USACO_08_OPEN_Clear_And_Present_Danger_S.cpp
+++ AcWaitd/Graph_Theory/Multi-Source_Shortest_Path/P_2910_USACO_08_OPEN_Clear_And_Present_Danger_S.cpp
@@ -5,10 +5,42 @@ typedef unsigned long long ull;
 typedef pair<int,int>PII;
 int INF=0x3f3f3f3f;
 const int N = 1e6 + 10;
+int f[110][110];
+int p[N];
+int n,m;
 int main() {
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  
+  cin>>n>>m;
+  for(int i=1;i<=m;i++)
+  {
+    cin>>p[i];
+  }
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=n;j++)
+    {
+      cin>>f[i][j];//k==0;
+    }
+  }
+  //floyd
+  ll ret=0;
+  for(int k=1;k<=n;k++)
+  {
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n;j++)
+        {
+            f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
+        }
+    }
+  }
+  for(int i=2;i<=m;i++)
+  {
+    int a=p[i-1],b=p[i];
+    ret+=f[a][b];
+  }
+  cout<<ret;
   return 0;
 }
