f32a58c1d94fe10504324c111d8303f2a9b2ca39
uh-uh-fw <3486039440@qq.com>
2026-06-13 23:51:36 +0800

    走上正轨

---
+++ AcWaitd/ABC/abc461a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int>PII;
+int INF=0x3f3f3f3f;
+#define lc p<<1
+#define rc p<<1|1
+const int N = 1e6 + 10;
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a<=b)cout<<"Yes";
+    else cout<<"No";
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;  // 多测时取消注释
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
